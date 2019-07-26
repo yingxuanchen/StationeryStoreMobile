c9a778d931d1ae81cbd1cc881e720c4c5fea2a2f
tws <sotong87@gmail.com>
2019-07-26 10:51:34 +0800

    update main activity

--- app/src/main/java/com/yingxuan/stationerystore/MainActivity.java
+++ app/src/main/java/com/yingxuan/stationerystore/MainActivity.java
@@ -5,27 +5,101 @@ import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.app.Activity;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.Toast;
+import com.yingxuan.stationerystore.ConnectionClass;
 
 import com.google.android.material.navigation.NavigationView;
 
-public class MainActivity extends AppCompatActivity {
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
-
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        ConnectionClass cClass = new ConnectionClass();
+        EditText edtUID = findViewById(R.id.et_username);
+        EditText edtPw = findViewById(R.id.et_password);
+        Button btnlogin = findViewById(R.id.btn_Login);
+        ProgressBar pgbar = findViewById(R.id.pbar);
+        pgbar.setVisibility(View.GONE);
 
+        btnlogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DoLogin doLogin = new DoLogin();
+                doLogin.execute("");
+            }
+        });
     }
 
+    public class DoLogin extends AsyncTask<String,String,String> {
+        String z = "";
+        Boolean isSuccess = false;
 
 
+        String userid = edtUID.getText().toString();
+        String password = edtPW.getText().toString();
+
+
+        @Override
+        protected void onPreExecute() {
+            pgbar.setVisibility(View.VISIBLE);
+        }
+
+        @Override
+        protected void onPostExecute(String r) {
+            pgbar.setVisibility(View.GONE);
+            Toast.makeText(MainActivity.this, r, Toast.LENGTH_SHORT).show();
+
+            if (isSuccess) {
+                Toast.makeText(MainActivity.this, r, Toast.LENGTH_SHORT).show();
+            }
+
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+            if (userid.trim().equals("") || password.trim().equals(""))
+                z = "Please enter User Id and Password";
+            else {
+                try {
+                    Connection con = cClass.Connect();
+                    if (con == null) {
+                        z = "Error in connection with SQL server";
+                    } else {
+                        String query = "select * from pathologic_app_users where username='" + userid + "' and password='" + password + "'";
+                        Statement stmt = con.createStatement();
+                        ResultSet rs = stmt.executeQuery(query);
+
+                        if (rs.next()) {
+
+                            z = "Login successfull";
+                            isSuccess = true;
+                        } else {
+                            z = "Invalid Credentials";
+                            isSuccess = false;
+                        }
+
+                    }
+                } catch (Exception ex) {
+                    isSuccess = false;
+                    z = "Exceptions";
+                }
+            }
+            return z;
+        }
+    }
 }
