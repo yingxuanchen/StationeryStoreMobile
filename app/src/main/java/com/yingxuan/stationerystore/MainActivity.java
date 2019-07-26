20cdae3df60cefdfb80d4d103b94250a34b5854e
Yingxuan Chen <e0021766@u.nus.edu>
2019-07-26 10:19:29 +0800

    Add navigation drawer

--- app/src/main/java/com/yingxuan/stationerystore/MainActivity.java
+++ app/src/main/java/com/yingxuan/stationerystore/MainActivity.java
@@ -16,54 +16,16 @@ import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private DrawerLayout dl;
-    private ActionBarDrawerToggle t;
-    private NavigationView nv;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        dl = findViewById(R.id.activity_main);
-        t = new ActionBarDrawerToggle(this, dl,R.string.open, R.string.close);
 
-        dl.addDrawerListener(t);
-        t.syncState();
-
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-
-        nv = findViewById(R.id.nv);
-        nv.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                int id = item.getItemId();
-                switch(id)
-                {
-                    case R.id.approve:
-                        Toast.makeText(MainActivity.this, "My Account",Toast.LENGTH_SHORT).show();break;
-                    case R.id.rep:
-                        Toast.makeText(MainActivity.this, "Settings",Toast.LENGTH_SHORT).show();break;
-                    case R.id.delegate:
-                        Toast.makeText(MainActivity.this, "My Cart",Toast.LENGTH_SHORT).show();break;
-                    case R.id.logout:
-                        break;
-                    default:
-                        return true;
-                }
-
-                return true;
-            }
-        });
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
 
-        if(t.onOptionsItemSelected(item))
-            return true;
-
-        return super.onOptionsItemSelected(item);
-    }
 
 }
