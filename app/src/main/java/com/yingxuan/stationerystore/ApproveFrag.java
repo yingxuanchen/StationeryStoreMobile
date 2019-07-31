eb42c64f19828dd4f89546a29488438197810474
tws <sotong87@gmail.com>
2019-07-31 14:12:36 +0800

    give you approve. Y-_-

---
+++ app/src/main/java/com/yingxuan/stationerystore/ApproveFrag.java
@@ -0,0 +1,32 @@
+package com.yingxuan.stationerystore;
+
+import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+
+public class ApproveFrag extends Fragment {
+
+    FragmentActivity listener;
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof Activity) {
+            this.listener = (FragmentActivity) context;
+        }
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.assignrep_frag, container, false);
+        return view;
+    }
+}
+
