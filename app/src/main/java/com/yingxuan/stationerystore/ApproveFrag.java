49bd86ea0b3ba9402cbbfd1ee269f3cb5d88ee81
tws <sotong87@gmail.com>
2019-07-31 14:33:43 +0800

    update activity_main.xml

--- app/src/main/java/com/yingxuan/stationerystore/ApproveFrag.java
+++ app/src/main/java/com/yingxuan/stationerystore/ApproveFrag.java
@@ -25,7 +25,7 @@ public class ApproveFrag extends Fragment {
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.assignrep_frag, container, false);
+        View view = inflater.inflate(R.layout.approvereq_frag, container, false);
         return view;
     }
 }
